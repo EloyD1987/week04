1f337ba48c78ac516a4b112512e801cf20c06fcf
Eloy Dordolo <eloydordolo87@gmail.com>
2020-10-25 18:04:23 -0300

    exercise-2-d

--- week04.js
+++ week04.js
@@ -40,6 +40,15 @@
     */
     /*c-Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los
      últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring).*/
+     /*
     var favouritefilm = "underworld";
     var last3letters = favouritefilm.substring(7,11);
     console.log(last3letters);
+    */
+    /*d-Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la 
+    primera letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable 
+    (utilizar substring, toUpperCase, toLowerCase y el operador +).*/
+    var favouritefilm = "underworld";
+    var Favouritefilm =  favouritefilm.substring(0,1).toUpperCase() + favouritefilm.substring(1,11).toLowerCase();
+    console.log(Favouritefilm);
+    
