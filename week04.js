e1bd1f402f97f6da7efb28978a0066996d9f99a4
Eloy Dordolo <eloydordolo87@gmail.com>
2020-10-25 16:23:14 -0300

    exercise-1-a

---
+++ week04.js
@@ -0,0 +1,12 @@
+/*1-Variables y Operadores*/
+    /*a-Crear dos variables numéricas y utilizar el operador suma para guardar el 
+    valor de la suma de ambos números en una 3er variable.*/
+    /*
+    var a = 5;
+    var b = 7;
+    var add = a + b;
+
+    console.log(add);
+    */
+ 
+
