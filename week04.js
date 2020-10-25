0832675bbc6c6f55a69b1b029245da67a07832b7
Eloy Dordolo <eloydordolo87@gmail.com>
2020-10-25 17:25:46 -0300

    execise-2-a

--- week04.js
+++ week04.js
@@ -5,19 +5,27 @@
     var a = 5;
     var b = 7;
     var add = a + b;
-
     console.log(add);
     */
-    /*Crear dos variables de tipo String y concatenarlas guardando el resultado en 
+    /*b-Crear dos variables de tipo String y concatenarlas guardando el resultado en 
     una 3er variable.*/
+    /*
     var name = "Eloy";
     var surname = " Dordolo";
     var fullname = name + surname;
     console.log(fullname);
-    /*Crear dos variables de tipo String y sumar el largo de cada variable 
+    */
+    /*c-Crear dos variables de tipo String y sumar el largo de cada variable 
     (cantidad de letras del string) guardando el resultado la suma una 3er variable (utilizar length).*/
+    /*
     var name = "Eloy";
     var surname = "Dordolo";
     var varlength = name.length + surname.length;
     console.log(varlength);
-      
+    */
+
+/*2-Strings
+    /*a-Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en 
+    mayúscula (utilizar toUpperCase).*/
+    var greeting = "good morning";
+    console.log(greeting.toUpperCase());
