a30a208857047f45d712d2361cc751451138cedf
Eloy Dordolo <eloydordolo87@gmail.com>
2020-10-26 08:43:01 -0300

    exercise 2-f and correction of 2-d

--- week04.js
+++ week04.js
@@ -50,12 +50,22 @@
     (utilizar substring, toUpperCase, toLowerCase y el operador +).*/
     /*
     var favouritefilm = "underworld";
-    var Favouritefilm =  favouritefilm.substring(0,1).toUpperCase() + favouritefilm.substring(1,11).
-    toLowerCase(); console.log(Favouritefilm);
+    var Favouritefilm =  favouritefilm.substring(0,1).toUpperCase() + favouritefilm.substring(1,11).toLowerCase();
+    console.log(Favouritefilm);
     */
     /*e- Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. 
     Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).*/
+    /*
     var greeting = "good morning";
     var spacepos = greeting.indexOf(" ");
     console.log(spacepos);
-    
+    */
+    /*f-Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio 
+    entre medio). Utilizar los métodos de los ejercicios anteriores para generar un nuevo string 
+    que tenga la primera letra de ambas palabras en mayúscula y las demás letras en minúscula 
+    (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).*/
+    var greeting = "good morning";
+    console.log(greeting.indexOf("m"));
+    var Greeting = greeting.substring(0,1).toUpperCase() + greeting.substring(1,4)+" "
+                   +greeting.substring(5,6).toUpperCase() + greeting.substring(6,12);
+    console.log(Greeting);
