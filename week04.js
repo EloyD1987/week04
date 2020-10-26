f19f8af03a752d4d3f74ef29265ca6494b49f414
Eloy Dordolo <eloydordolo87@gmail.com>
2020-10-26 09:05:02 -0300

    exercise 3-b

--- week04.js
+++ week04.js
@@ -75,7 +75,13 @@
     /*Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", 
     "Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11 
     (utilizar console.log)*/
+    /*
     var months = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre",
                     "Octubre","Noviembre","Diciembre"]
     console.log(months[4], months[10]);
-    
+    */
+    /*b- Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).*/
+    var months = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre",
+                   "Octubre","Noviembre","Diciembre"]
+    console.log(months.sort());
+
