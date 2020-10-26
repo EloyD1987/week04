7488a1559fa6e3fc6134d872558fef776feb23c6
Eloy Dordolo <eloydordolo87@gmail.com>
2020-10-26 17:00:10 -0300

    exercise 5d

--- week04.js
+++ week04.js
@@ -159,9 +159,20 @@
   /*c-Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto 
   a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence. 
   Al final mostrar una única alerta con la cadena completa.*/
-
+  /*
   var sentence = [" "];
   for ( var i = 0; i < Weekdays.length ; i++){
       sentence[i] = Weekdays[i];
   }
   alert(sentence);
+  */
+  /*d-Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número 
+  de la repetición, es decir que al final de la ejecución del bucle for deberia haber 10 elementos
+  dentro del array, desde el número 0 hasta al numero 9. Mostrar por la consola del navegador el 
+  al array final (utilizar console.log).*/
+  
+  var numbers = [" "];
+  for ( var i = 0; i < 10 ; i++){
+      numbers[i] =  i;
+  }
+  console.log(numbers);
